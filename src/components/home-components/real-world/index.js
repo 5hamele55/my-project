14b256c49877026cd46c53a9f624dd385e0d894f
5hamele55 <roman.zhuk1993@gmail.com>
2022-01-12 16:42:10 +0200

    added realworld state

--- src/components/home-components/real-world/index.js
+++ src/components/home-components/real-world/index.js
@@ -1,43 +1,35 @@
 import style from './style.css';
+import { useState } from 'preact/hooks';
 
-const RealWorld = ({ scroll }) => (
-  <div class={style.realWorld}>
-    <div class={`${style.realWorldShape} ${scroll > 70 && style.realWorldShapeSlideIn}`}></div>
-    <div class={style.realWorldContainer}>
-      <div class={style.realWorldTitle}>
-        <h2>real-world experience</h2>
-        <span>The best business consulting firm you can count on!</span>
-      </div>
-      <div class={style.realWorldRow}>
-        <div class={style.realWorldCol}>
-          <div class={style.realWorldColInner}>
-            <h2>
-              <a href="#">wealth <br /> Management</a>
-            </h2>
-            <a class={style.realWorldLink} href='#'>Read More</a>
-            <div class={style.realWorldColIcon}><span>1</span></div>
-          </div>
+const RealWorld = ({ scroll }) => {
+  const [realItems, setRealItems] = useState([
+    { link: '#', body1: 'wealth', body2: 'Management', icon: 1, readMoreLink: '#' },
+    { link: '#', body1: 'audit', body2: 'marketing', icon: 2, readMoreLink: '#' },
+    { link: '#', body1: 'Finance', body2: 'consulting', icon: 3, readMoreLink: '#' }
+  ])
+  return (
+    <div class={style.realWorld}>
+      <div class={`${style.realWorldShape} ${scroll > 70 && style.realWorldShapeSlideIn}`}></div>
+      <div class={style.realWorldContainer}>
+        <div class={style.realWorldTitle}>
+          <h2>real-world experience</h2>
+          <span>The best business consulting firm you can count on!</span>
         </div>
-        <div class={style.realWorldCol}>
-          <div class={style.realWorldColInner}>
-            <h2>
-              <a href="#">wealth <br /> Management</a>
-            </h2>
-            <a class={style.realWorldLink} href='#'>Read More</a>
-            <div class={style.realWorldColIcon}><span>1</span></div>
-          </div>
-        </div>
-        <div class={style.realWorldCol}>
-          <div class={style.realWorldColInner}>
-            <h2>
-              <a href="#">wealth <br /> Management</a>
-            </h2>
-            <a class={style.realWorldLink} href='#'>Read More</a>
-            <div class={style.realWorldColIcon}><span>1</span></div>
-          </div>
+        <div class={style.realWorldRow}>
+          {realItems.map(item => (
+            <div class={style.realWorldCol}>
+              <div class={style.realWorldColInner}>
+                <h2>
+                  <a href={item.link}>{item.body1}<br />{item.body2}</a>
+                </h2>
+                <a class={style.realWorldLink} href={item.readMoreLink}>Read More</a>
+                <div class={style.realWorldColIcon}><span>{item.icon}</span></div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 export default RealWorld;
